372dae5b05f88ef6d64b0851ef8f53085f849ec8
Du Mesnildot Laure <laure.du-mesnildot@insa-rennes.fr>
2019-01-16 19:05:40 +0100

    front

--- src/web_interface/frontend/src/app/app-routing.module.ts
+++ src/web_interface/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+  redirectTo: 'accueil',
+  pathMatch: 'full'
+  },
+  {path: 'accueil', component: AccueilComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/web_interface/frontend/src/app/app.module.ts
+++ src/web_interface/frontend/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
