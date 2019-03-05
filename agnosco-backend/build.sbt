791762a09e312fdff8e98d2f46b5806253e1e875
cloudyhug <cloudyhug@protonmail.ch>
2019-03-05 16:00:49 +0100

    corrected build.sbt, added simple socket to read answer from line detector

--- agnosco-backend/build.sbt
+++ agnosco-backend/build.sbt
@@ -7,7 +7,7 @@ scalaVersion := "2.12.8"
 libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.1.1"
 libraryDependencies += "org.scalatest" % "scalatest_2.12" % "3.0.5" % "test"
 
-libraryDependencies += "org.glassfish.hk2" % "javax.inject" % "2.5.0"
+libraryDependencies += "org.glassfish.hk2.external" % "javax.inject" % "2.5.0-b62"
 libraryDependencies += "org.glassfish.grizzly" % "grizzly-http-server" % "2.4.4"
 libraryDependencies += "org.glassfish.jersey.core" % "jersey-server" % "2.28"
 libraryDependencies += "org.glassfish.jersey.containers" % "jersey-container-grizzly2-http" % "2.28"
--- agnosco-backend/src/main/scala/model/preparation/processing/linedetection/BlurLineDetector.scala
+++ agnosco-backend/src/main/scala/model/preparation/processing/linedetection/BlurLineDetector.scala
@@ -2,78 +2,29 @@ package model.preparation.processing.linedetection
 
 import java.io._
 import java.net.{InetSocketAddress, Socket}
-import java.nio.channels.{ServerSocketChannel, SocketChannel}
+import java.nio.channels.SocketChannel
 
 import model.preparation.types.Line
 
-import scala.collection.mutable.ListBuffer
-import scala.io.Source
-
-class BlurLineDetector(backendIp : String, detectorIp : String, port : Int) extends LineDetector {
+class BlurLineDetector(detectorIp : String, filePort : Int, answerPort : Int) extends LineDetector {
   override def detectLines(src : String) : List[Line] = {
-    val ss = ServerSocketChannel.open()
-    ss.bind(new InetSocketAddress(backendIp, port))
 
-    val sc = SocketChannel.open(new InetSocketAddress(detectorIp, port))
+    val fileSocketChannel = SocketChannel.open(new InetSocketAddress(detectorIp, filePort))
+    val answerSocket = new Socket(detectorIp, answerPort)
 
     // send the file to the line detector
-    val fc = new FileInputStream(src).getChannel
-    fc.transferTo(0, fc.size(), sc)
-    fc.force(true)
-    fc.close()
-    sc.close()
+    val fileChannel = new FileInputStream(src).getChannel
+    fileChannel.transferTo(0, fileChannel.size(), fileSocketChannel)
+    fileChannel.force(true)
+    fileChannel.close()
+    fileSocketChannel.close()
     println("[INFO] File sent to the line detector")
 
-    // read the answer from the server into a 'tmp' file
-    println("[INFO] Waiting for the answer...")
-    println(ss.getLocalAddress)
-
-    val scAnswer = ss.accept()
-    println("[INFO] Connection incoming from the line detector")
-
-    val fcAnswer = new FileOutputStream("tmp").getChannel
-    fcAnswer.transferFrom(scAnswer, 0, Long.MaxValue)
-    fcAnswer.force(true)
-    fcAnswer.close()
-    scAnswer.close()
+    // read the answer from the server
+    val answerReader = new BufferedReader(new InputStreamReader(answerSocket.getInputStream))
+    val answer = answerReader.readLine() + answerReader.readLine()
+    answerSocket.close()
     println("[INFO] Answer received from the line detector")
-
-    val answer = Source.fromFile("tmp").getLines()
-
-    // delete the temporary file
-    new File("tmp").delete()
-
-    /*// read file corresponding to 'src' path into a buffer
-    val inputFile = new File(src)
-    val buffer = new Array[Byte](inputFile.length().asInstanceOf[Int])
-    val bis = new BufferedInputStream(new FileInputStream(inputFile))
-    bis.read(buffer, 0, buffer.length)
-    println("[INFO] File read into a buffer")
-
-    val fileSizeAsArray = BigInt(buffer.length).toByteArray // buffer length as Array[Byte]
-
-    // open socket and write data to it
-    val socket = new Socket(host, port)
-    val os = socket.getOutputStream
-    os.write(fileSizeAsArray)
-    os.write(buffer, 0, buffer.length)
-    os.flush()
-    println("[INFO] File sent to the line detector")*/
-
-    /*// read server answer
-    val is = new BufferedReader(new InputStreamReader(socket.getInputStream))
-    val answerBuffer = new ListBuffer[String]
-
-    var inputOk = true
-    while (inputOk) {
-      try {
-        answerBuffer += is.readLine()
-      } catch {
-        case e : IOException => inputOk = false
-      }
-    }*/
-
-    //println("[INFO] Answer received from the line detector")
 
     // operations on the answer to get Line objects
     // TODO
