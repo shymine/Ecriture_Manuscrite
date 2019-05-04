07e4bdd6d0fd16d6df4e4aa8f208702c2b98f4bb
Charicha The Great <cornichonsuperstar@gmail.com>
2019-05-04 17:58:16 +0200

    gestion des pages nouveau ++

--- agnosco-frontend/src/app/gestion-pages/gestion-pages.component.ts
+++ agnosco-frontend/src/app/gestion-pages/gestion-pages.component.ts
@@ -30,9 +30,8 @@ export class GestionPagesComponent implements OnInit {
   */
 
   public pages = [];
-  public newpages = [];
-  public cancelledpages = [];
-  public modifypages = [];
+  public oldpages = [];
+
   public pid = -1;
   public did = -1;
 
@@ -57,6 +56,7 @@ export class GestionPagesComponent implements OnInit {
       Object.keys(returnedData).forEach( key => {
         console.log("key");
         console.log(returnedData[key]);
+        this.oldpages.push({'nameIm':returnedData[key].name, 'nameVt': returnedData[key].image64, 'modified': 0, cancelled: 0}); //
       });
     });
   }
@@ -71,14 +71,21 @@ export class GestionPagesComponent implements OnInit {
 
     const event = param[0];
     const page = param[1];
+    const tab = param[2];
 
     const file = event.target.files[0];
     console.log(file)
     const reader = new FileReader();
     const http = this.http;
-    const pages = this.pages;
 
-    const isM = this.isModified(page);
+    let pages;
+
+    if(tab == "old"){
+      pages = this.oldpages;
+      this.oldpages[page].modified = 1;
+    }else{
+      pages = this.pages;
+    }
 
     reader.onloadend = function() {
       let res: string = reader.result as string;
@@ -87,7 +94,11 @@ export class GestionPagesComponent implements OnInit {
         encoded += '='.repeat(4-(encoded.length%4));
       }
 
+      console.log("file.name");
+      console.log(file.name);
+
       pages[page].image = encoded;
+      pages[page].name = file.name;
 
       
       console.log("encoded");
@@ -96,13 +107,8 @@ export class GestionPagesComponent implements OnInit {
       console.log(pages[page]);
     }
 
-    if(isM >= 0){
-      this.modifypages.push(isM);
-    }
-
     reader.readAsDataURL(file);
     console.log("j'encode");
-    
   }
 
   encodeDataFile(param) {
@@ -112,6 +118,7 @@ export class GestionPagesComponent implements OnInit {
 
     const event = param[0];
     const page = param[1];
+    const tab = param[2];
 
     console.log("page");
     console.log(page);
@@ -119,34 +126,31 @@ export class GestionPagesComponent implements OnInit {
     const file = event.target.files[0];
     console.log(file)
 
-    const isM = this.isModified(page);
-
     const reader = new FileReader();
     const http = this.http;
-    const pages = this.pages;
+
+    let pages;
+
+    if(tab == "old"){
+      pages = this.oldpages;
+      this.oldpages[page].modified = 1;
+    }else{
+      pages = this.pages;
+    }
 
     reader.onloadend = function() {
       let res: string = reader.result as string;
 
       console.log("encoded");
 
-      console.log("file.name");
-      console.log(file.name);
-
-      pages[page].data = res;
-      pages[page].name = file.name;
-
-      
+      pages[page].data = res;      
 
       console.log("pages[page]");
       console.log(pages[page]);
     }
+
     reader.readAsText(file);
     console.log("j'encode");
-
-    if(isM >= 0){
-      this.modifypages.push(isM);
-    }
   }
 
   onNoClick(): void {
@@ -166,19 +170,14 @@ export class GestionPagesComponent implements OnInit {
   onValidation(): void {
 
     console.log("validation");
-    console.log("pages");
-    console.log(this.pages);
 
     console.log("voilà ce qui va se passer!");
 
-    console.log(" X à modifier X ");
-    console.log(this.modifypages);
-    
-    console.log(" X à supprimer X ");
-    console.log(this.cancelledpages);
+    console.log("les vieilles et leurs etats");
+    console.log(this.oldpages);
 
     console.log(" X à ajouter X ");
-    console.log(this.newpages);
+    console.log(this.pages);
 
     /* 1) si pas à supprimer : modifier 2) supprimer 3) ajouter */
 
@@ -189,143 +188,35 @@ export class GestionPagesComponent implements OnInit {
   plusPage(){
     console.log("add one page");
     
-    this.newpages.push(this.pages.length);
     this.pages.push({'name':"default", 'image': "default", 'data':"default"});
     
     console.log(this.pages);
   }
 
-  // page = index dans pages
-  deletePage(page){
-    console.log("DELETE ONE PAGE");
+  deleteOld(page){
+    //page = index dans oldpages
+    console.log("DELETE OLD PAGE");
 
-    let m = this.isCancelled(page);
-
-    console.log("M");
-    console.log(m);
-
-    if(m >= 0){
-
-      console.log("Actually.. Restaure it");
-      this.restaurer(m);
+    if(this.oldpages[page].cancelled == 1){
+      console.log("RESTAURATION");
+      this.oldpages[page].cancelled = 0;
 
     }else{
-
-      let n = this.isNew(page);
-      console.log("N");
-      console.log(n);
-
-      /* La page vient juste d'être ajoutée => on la supprime */
-      if(n >= 0){
-        this.annulationAjout(n);
-        //et suppression dans pages
-        for (var _j = page; _j < this.pages.length-1; _j++){
-          this.pages[_j] = this.pages[_j+1];
-        }
-        this.pages.pop();
-
-      /* la page est ancienne => on la rajoute aux pages à supprimer
-      /!\ on ne les retire pas de la liste, on les grise (peuvent etre recpérées)
-      puisque non supprimées: pas besoin de décaler les indices dans news */
-      }else{
-        console.log("ajout des pages à supprimer");
-        this.cancelledpages.push(page);
-        console.log("objets à supprimer:");
-        console.log(this.cancelledpages);
-      }
-
+      console.log("SUPPRESSION");
+      this.oldpages[page].cancelled = 1;
+      
     }
-
-    console.log("end of delete page");
-    console.log("pages");
-    console.log(this.pages);
-    console.log("news");
-    console.log(this.newpages);
-    console.log("cancelled");
-    console.log(this.cancelledpages);
-    
   }
 
-/* p est l'index de la liste des pages */
+  deleteNew(page){
+    //page = index dans pages
 
-  //retirer de cancelledpages
-  restaurer(n){
-    console.log("annulation de la suppression");
-    console.log("page: "+n);
+    console.log("DELETE NEW PAGE");
+    console.log("annulation de l'ajout => suppression dans pages");
 
-    for (var _i = n; _i < this.cancelledpages.length-1; _i++){
-      console.log("_i");
-      console.log(_i);
-      console.log("avant: "+this.cancelledpages[_i]);
-      console.log("mtn: "+this.cancelledpages[_i+1]);
-      this.cancelledpages[_i] = this.cancelledpages[_i+1];
+    for (var _j = page; _j < this.pages.length-1; _j++){
+      this.pages[_j] = this.pages[_j+1];
     }
-    this.cancelledpages.pop();
-    console.log("objets à ajouter:");
-    console.log(this.cancelledpages);
-  }
-
-  annulationAjout(n){
-    console.log("annulation de l'ajout");
-    console.log("page: "+n);
-
-    for (var _i = n; _i < this.newpages.length-1; _i++){
-      console.log("_i");
-      console.log(_i);
-      console.log("avant: "+this.newpages[_i]);
-      console.log("normalement: "+this.newpages[_i+1]);
-      this.newpages[_i] = this.newpages[_i+1]-1;
-    }
-    this.newpages.pop();
-    console.log("objets à ajouter:");
-    console.log(this.newpages);
-  }
-
-  //retourne son index dans newpages sinon -1
-  isNew(p){
-    console.log("IS NEW?");
-    console.log("p: "+p);
-    console.log("les news");
-    console.log(this.newpages);
-    for(var _i = 0; _i < this.newpages.length ; _i++){
-      if(this.newpages[_i] == p){
-        console.log(_i);
-        return _i;
-      }
-    }
-    console.log(-1);
-    return -1;
-  }
-
-  //retourne son index dans cancelledpages sinon -1
-  isCancelled(p){
-    console.log("IS CANCELLED?");
-    console.log("p: "+p);
-    console.log("les cancelled");
-    console.log(this.cancelledpages);
-    for(var _i = 0; _i < this.cancelledpages.length ; _i++){
-      if(this.cancelledpages[_i] == p){
-        console.log(_i);
-        return _i;
-      }
-    }
-    console.log(-1);
-    return -1;
-  }
-
-  //retourne son index dans modifypages sinon -1
-  isModified(p){
-    console.log("IS MODIFIED?");
-    console.log("p: "+p);
-    console.log("les modified");
-    console.log(this.modifypages);
-    for(var _i = 0; _i < this.modifypages.length ; _i++){
-      if(this.modifypages[_i] == p){
-        console.log(_i);
-        return _i;
-      }
-    }
-    console.log(-1);
-    return -1;
+    this.pages.pop();
   }
 }
