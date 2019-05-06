bef153f95102ce6d2e6fa208f5730b8e07ff40eb
shymine <timothee.neitthoffer@gmail.com>
2019-05-06 10:32:50 +0200

    working on  examples

--- agnosco-backend/src/main/scala/model/preparation/processing/BaseExampleMaker.scala
+++ agnosco-backend/src/main/scala/model/preparation/processing/BaseExampleMaker.scala
@@ -6,36 +6,40 @@ import model.preparation.input.piff.PiFF
 import scala.collection.mutable.ListBuffer
 
 object BaseExampleMaker extends ExampleMaker {
-  override def makeExamples(p : PiFF): List[Example] = {
-    val examples = new ListBuffer[Example]
+	override def makeExamples(p : PiFF): List[Example] = {
+		val examples = new ListBuffer[Example]
 
-    /*
-    * The example id will be -1 so that the database automatically generates a unique id, but
-    * we need a way to make sure the examples are associated with a unique image filename.
-    * imageId is a way to ensure that. It is just an integer being incremented as we create examples.
-    */
-    var imageId = 0
+		/*
+		* The example id will be -1 so that the database automatically generates a unique id, but
+		* we need a way to make sure the examples are associated with a unique image filename.
+		* imageId is a way to ensure that. It is just an integer being incremented as we create examples.
+		*/
+		var imageId = 0
 
-    val page = p.page
+		val page = p.page
 
-    page.elements.foreach(element => {
-      val transcript =
-        element.contents match {
-          case "" => None
-          case c => Some(c)
-        }
+		page.elements.foreach(element => {
+			val transcript =
+				element.contents match {
+					case "" => None
+					case c => Some(c)
+				}
 
-      // new image from the polygon
-      val imgPath = s"$globalDataFolder/${page.src}"
-      println("img path : " + imgPath)
-      val newExampleImgPath = ImageProcessing.createThumbnail(imgPath, imageId, element.polygon)
+			// new image from the polygon
+			val imgPath = s"$globalDataFolder/${page.src}"
+			println("img path : " + imgPath)
+			val newExampleImgPath = ImageProcessing.createThumbnail(imgPath, imageId, element.polygon)
 
-      // adding the newly created example to the list
-      examples += Example(-1, newExampleImgPath, transcript)
+			// adding the newly created example to the list
+			examples += Example(-1, removeDataFolderPath(newExampleImgPath), transcript)
 
-      imageId += 1
-    })
+			imageId += 1
+		})
 
-    examples.toList
-  }
+		examples.toList
+	}
+
+	private def removeDataFolderPath(path: String): String = {
+		"data/".r.replaceAllIn(path, "")
+	}
 }
