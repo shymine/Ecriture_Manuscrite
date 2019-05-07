bfc5947a19732231e3b67fddb39ecdc81c2c1e8b
Timothée <timothee.neitthoffer@gmail.com>
2019-05-07 16:30:51 +0200

    tmp

--- agnosco-backend/src/main/scala/model/recogniser/Recogniser.scala
+++ agnosco-backend/src/main/scala/model/recogniser/Recogniser.scala
@@ -7,29 +7,6 @@ import org.json.JSONObject
   * The trait of a recognizer and the actions it can perform
   */
 trait Recogniser {
-  /**
-    * Train the Recognizer using the samples given in parameter
-    * @param samples The samples the Recognizer trains on
-    */
-  def train(samples: Iterable[Example]): Unit
-
-  /**
-    * Evaluate the Recognizer performance on a test set of Sample
-    * @param samples The set to test the Recognizer
-    */
-  def evaluate(samples: Iterable[Example]): JSONObject
-
-  /**
-    * Production mode of the Recognizer
-    * @param samples The samples the Recognizer must "translate".
-    *                It's format as a list to ease the process of
-    *                knowing the correct translation to the image
-    * @return Returns the List of the sample corresponding to the
-    *         image and the translation. If the input format is a
-    *         paragraph then there may be n samples extracted from
-    *         it depending if the Recognizer output for a paragraph
-    *         is an output per line or one output for the paragraph.
-    */
-  def recognize(samples: Iterable[Example]) : Iterable[Example]
+  def export
 
 }
