37c3757118eb70716524645f51e413d164131abd
shymine <timothee.neitthoffer@gmail.com>
2019-05-08 20:59:00 +0200

    creating symbol table

--- prepareLaia.py
+++ prepareLaia.py
@@ -67,6 +67,39 @@ testTxt = open("%s/test.txt" % datasetExport, "w")
 trainLst = open("%s/train.lst" % datasetExport, "w")
 trainTxt = open("%s/train.txt" % datasetExport, "w")
 
+with open("%s/symbs.txt" % datasetExport, "w") as file:
+    file.write("""
+    <eps> 0
+    <ctc> 1
+    a 2
+    b 3
+    c 4
+    d 5
+    e 6
+    f 7
+    g 8
+    h 9
+    i 10
+    j 11
+    k 12
+    l 13
+    m 14
+    n 15
+    o 16
+    p 17
+    q 18
+    r 19
+    s 20
+    t 21
+    u 22
+    v 23
+    w 24
+    x 25
+    y 26
+    z 27
+    {space} 30
+    """)
+
 for txtName in txtFiles:
     try:
         test = random() <= testRate
