5af884daa9b0807af15f3f646fe16c519d8524e6
shymine <timothee.neitthoffer@gmail.com>
2019-05-09 10:42:36 +0200

    reset to pseudo original maurdor base and correct png converter

--- agnosco-backend/src/main/scala/model/common/package.scala
+++ agnosco-backend/src/main/scala/model/common/package.scala
@@ -3,7 +3,8 @@ package model
 package object common {
   val globalDataFolder = "data"
   val globalExportFolder = "export"
-  val pythonImageCropperExecutablePath = "/media/laolao/Data1/DOCUMENTS/INSA/4ANNEE/Projet_Couasnon/Ecriture_Manuscrite/imagecropper.py"
+  val pythonImageCropperExecutablePath = "/home/timothee/Documents/Workspace/Ecriture_Manuscrite/Ecriture_Manuscrite/imagecropper.py"
+  //"/media/laolao/Data1/DOCUMENTS/INSA/4ANNEE/Projet_Couasnon/Ecriture_Manuscrite/imagecropper.py"
   val detectorIp = "192.168.1.12"
   val filePort = 7007
   val answerPort = 7008
